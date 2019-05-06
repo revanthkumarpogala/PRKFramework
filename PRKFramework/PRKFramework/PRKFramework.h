b9db84fddba932918cf841eb9d5c4aa1e2c695eb
Revanth Kumar Pogala <revanth.pogala@verizonconnect.com>
2019-05-06 16:20:42 +0530

    first committ

---
+++ PRKFramework/PRKFramework/PRKFramework.h
@@ -0,0 +1,19 @@
+//
+//  PRKFramework.h
+//  PRKFramework
+//
+//  Created by Revanth Kumar Pogala on 06/05/19.
+//  Copyright © 2019 Revanth Kumar Pogala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PRKFramework.
+FOUNDATION_EXPORT double PRKFrameworkVersionNumber;
+
+//! Project version string for PRKFramework.
+FOUNDATION_EXPORT const unsigned char PRKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PRKFramework/PublicHeader.h>
+
+
---
+++ PRKFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  PRKFramework
+//
+//  Created by Revanth Kumar Pogala on 06/05/19.
+//  Copyright © 2019 Revanth Kumar Pogala. All rights reserved.
+//
+
+import Foundation
+
+public class service{
+    private init(){}
+    
+    public static func doSomething() -> String{
+        return "asdfghjkl"
+    }
+    
+}
